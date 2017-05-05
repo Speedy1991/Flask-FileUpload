e0b898fa5f5d54c465059d470ad70b5938e4492b
Arthur Holzner <arthur.holzner@hs-augsburg.de>
2017-05-05 10:56:18 +0200

    finished new login stuff

--- example/app/app.py
+++ example/app/app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_fileupload import FlaskFileUpload
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
 app = Flask(__name__)
 app.config.from_object("config")
-
+lm = LoginManager(app)
 fue = FlaskFileUpload(app)
 
 
-@fue.auth_loader
-def auth_loader():
-    pass
-    #return redirect("https://google.de")
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
 
+
+@lm.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+@app.route("/login/")
+def login():
+    user = User("testuser")
+    login_user(user)
+    return redirect("/upload")
+
+
+@app.route("/logout/")
+def logout():
+    logout_user()
+    return redirect("/")
