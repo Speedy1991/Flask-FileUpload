3b867f17ab99ad68b0f89d03adabdc8e8738e23f
Arthur Holzner <arthur.holzner@hs-augsburg.de>
2017-07-11 11:39:40 +0200

    init

--- flask_fileupload/storage.py
+++ flask_fileupload/storage.py
@@ -1,6 +1,6 @@
 from werkzeug.utils import secure_filename
 import os
-import re
+from .utils import convert_to_snake_case
 
 
 class StorageNotAllowed(Exception):
@@ -18,37 +18,48 @@ class StorageNotExists(Exception):
         super(StorageNotExists, self).__init__("File does not exist")
 
 
-class Storage(object):
-
+class AbstractStorage(object):
     def __init__(self, app):
         self.allowed = app.config.get("FILEUPLOAD_ALLOWED_EXTENSIONS", list())
         self.all_allowed = app.config.get("FILEUPLOAD_ALLOW_ALL_EXTENSIONS", False)
         self.img_folder = app.config.get("FILEUPLOAD_IMG_FOLDER", "upload")
         self.snake_case = app.config.get("FILEUPLOAD_CONVERT_TO_SNAKE_CASE", False)
-
-        self.root = app.root_path
-        self.abs_img_folder = os.path.join(self.root, "static", self.img_folder)
-
         self.app = app
 
+    def get_existing_files(self):
+        raise NotImplementedError()
+
+    def store(self, filename, file_data):
+        raise NotImplementedError()
+
+    def delete(self, filename):
+        raise NotImplementedError()
+
+
+class Storage(AbstractStorage):
+
+    def __init__(self, app):
+        super(Storage, self).__init__(app)
+        self.abs_img_folder = os.path.join(app.root_path, "static", self.img_folder)
         if not os.path.exists(self.abs_img_folder):
             os.makedirs(self.abs_img_folder)
 
     def get_existing_files(self):
         return [f for f in os.listdir(self.abs_img_folder)]
 
+    def _get_abs_file(self, filename):
+        return os.path.join(self.abs_img_folder, filename)
+
     def store(self, filename, file_data):
         filename = secure_filename(filename)
-
         if self.snake_case:
-            filename = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', filename)
-            filename = re.sub('([a-z0-9])([A-Z])', r'\1_\2', filename).lower()
+            filename = convert_to_snake_case(filename)
 
         if filename in self.get_existing_files():
             raise StorageExists()
 
         if self.all_allowed or any(filename.endswith('.' + x) for x in self.allowed):
-            file_data.save(os.path.join(self.abs_img_folder, filename))
+            file_data.save(self._get_abs_file(filename))
         else:
             raise StorageNotAllowed()
 
@@ -59,7 +70,4 @@ class Storage(object):
         if filename not in existing_files:
             raise StorageNotExists()
         else:
-            os.remove(os.path.join(self.abs_img_folder, filename))
-
-    def get_abs_img_path(self):
-        return self.abs_img_folder
+            os.remove(self._get_abs_file(filename))
