80889697bfaf1e276345475a3f578442b3713db6
Arthur Holzner <arthur.holz.91@gmail.com>
2017-08-07 11:02:29 +0200

    updated to v.0.5.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(suffix=''):
 
 setup(
     name='Flask-FileUpload',
-    version="0.4.2",
+    version="0.5.0",
     url='https://github.com/speedy1991/Flask-FileUpload',
     license='MIT',
     author='Arthur Holzner',
